61959ad9f9b962f48f378eb25ab0e630e27430ce
Chiraag Gohel <cgohel@gmail.com>
2018-04-26 15:05:43 -0500

    chiraag additions to lin al

--- linear_algebra.py
+++ linear_algebra.py
@@ -33,16 +33,42 @@ def shape(A):
 
 def get_row(A, i):
 
+    row_list = []
+    for x in range(0, len(A[i])):
+        row_list.append(A[i][x])
+
+    return row_list
 
 def get_column(A, j):
 
+    colum_list = []
+    for x in range(0, len(A[j]):
+        colum_list.append(A[x][j])
+
+    return colum_list
 
 def make_matrix(num_rows, num_cols, entry_fn):
 
 
-def is_diagonal(i, j):
+def is_identity(A):
 
+    for x in range(0, len(A)):
+        for y in range(0, len(A[x])):
+            if x == y:
+                if A[x][y] != 1:
+                    return False
+            if x != y:
+                if A[x][y] != 0:
+                    return False
+
+    return True
 
 def matrix_add(A, B):
 
+    w, h = len(A[0]), len(A);
+    new_matrix = [[0 for x in range(w)] for y in range(h)]
+    for x in range (0, len(A)):
+        for y in range (0, len(A[x])):
+            new_matrix[x][y] = A[x][y] + B[x][y]
 
+    return new_matrix
