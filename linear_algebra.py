e612ac090c3de639804852318abfcb8170b37455
Elliot <e99pickens@gmail.com>
2018-04-26 14:42:00 -0500

    update

---
+++ linear_algebra.py
@@ -0,0 +1,48 @@
+def add_vectors(v, w):
+
+
+def subtract_vectors(v, w):
+
+
+def vector_sum(vecs):
+
+
+def scalar_multiplication(c, v):
+
+
+def vector_mean(vecs):
+
+
+def dot(v, w):
+
+
+def sum_of_sqrs(v):
+
+
+def magnitude(v):
+
+
+def squared_distance(v, w):
+
+
+def distance(v, w):
+
+
+def shape(A):
+
+
+def get_row(A, i):
+
+
+def get_column(A, j):
+
+
+def make_matrix(num_rows, num_cols, entry_fn):
+
+
+def is_diagonal(i, j):
+
+
+def matrix_add(A, B):
+
+
