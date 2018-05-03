6b0a6686aa06fffd03436f0c4acca0d247d84fb5
Elliot <e99pickens@gmail.com>
2018-05-03 14:42:13 -0500

    new stuff

---
+++ probability.py
@@ -0,0 +1,32 @@
+from collections import Counter
+import math, random
+
+def uniform_pdf(x):
+    if (x < 1 and x >= 0):
+        return 1
+    else:
+        return 0
+
+
+
+def uniform_cdf(x):
+
+
+
+def normal_pdf(x, mu=0, sigma=1):
+
+
+
+def normal_cdf(x, mu=0, sigma=1):
+
+
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+
+
+
+def bernoulli_trial(p):
+
+
+
+def binomial(p, n):
---
+++ statistics.py
@@ -0,0 +1,38 @@
+def mean(x):
+
+
+def median(x):
+
+
+def quantile(x):
+
+
+def mode(x):
+
+
+def range(x):
+
+
+#subtract the mean from each data point
+def dif_means(x):
+
+
+#variance
+def var(x):
+
+
+def std_dev(x):
+
+
+#difference between quartiles
+def quartile_range(x):
+
+
+#covariance
+def covar(x, y):
+
+
+#correlation
+def corr(x, y):
+
+    
