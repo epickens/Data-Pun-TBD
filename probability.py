f323a5a5935627ea6552edda89d98e902b782174
Elliot <e99pickens@gmail.com>
2018-05-07 03:52:21 -0500

    added inverse norm, geometric, and gamma distros

--- probability.py
+++ probability.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import math, random
 from scipy.stats import norm
+from scipy.special import gammainc
 
 
 def uniform_pdf(x):
@@ -31,7 +32,26 @@ def normal_cdf(x, mu=0, sigma=1):
 
 
 def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
-    norm.ppf(p)
+    #check for rescaling
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+
+    low_z_score = -15
+    high_z_score = 15
+    avg_z_score = (low_z_score + high_z_score) / 2
+
+    while high_z_score - low_z_score > tolerance:
+        avg_pval = normal_cdf(avg_z_score)
+        avg_z_score = (low_z_score + high_z_score) / 2
+
+        if avg_pval > p:
+            high_z_score = avg_z_score
+        elif avg_pval < p:
+            low_z_score = avg_z_score
+        else:
+            break
+
+    return avg_zscore
 
 
 def bernoulli_trial(p):
@@ -46,6 +66,22 @@ def binomial(p, n):
     return sum
 
 
+def geometric_pdf(p, k, failures=False):
+    if failures:
+        exp_term = (1- p) ^ k
+        return exp_term * p
+    else:
+        exp_term = (1 - p) ^ (k -1)
+        return exp_term * p
+
+
+def geometric_cdf(p, k, failures=False):
+    if failures:
+        return 1 - (1 - p) ^ (k + 1)
+    else:
+        return 1 - (1 - p) ^ k
+
+
 def poisson_pdf(lam, k):
     return lam ^ k * math.exp(-lam)/math.factorial(k)
 
@@ -56,3 +92,24 @@ def poisson_cdf(lam, x):
         cdf += poisson_pdf(lam, x)
 
     return cdf
+
+
+def gamma_function(n):
+    return math.factorial(n-1)
+
+
+def gamma_pdf(k, theta, x):
+    coef = 1 / (gamma_function(k) * (theta) ^ k)
+    e_term = math.exp(- (x / theta))
+    x_term = x ^ (k - 1)
+
+    return coef * x_term * e_term
+
+
+def gamma_cdf(k, theta, x):
+    #gamma = 1 / gamma_function(k)
+    #incomp_gamma = incomp_gamma_function(a, k)
+    #need to implement a version of the imcomplete gamma function
+    #but I'm not sure how to rn
+    #so I'm taking the easy way out
+    return gammainc(k, x / theta)
